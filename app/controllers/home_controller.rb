a640a5dbdfc4ebe4ce34ea75c4366529ba7ab15c
remy727 <remy.wang727@gmail.com>
2022-09-02 12:27:31 -0400

    Feat: add Turbo Frame usage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,11 @@
 class HomeController < ApplicationController
+  # @route GET /turbo_frame_form
+  def turbo_frame_form
+  end
+
+  # @route POST /turbo_frame_submit
+  def turbo_frame_submit
+    extracted_anynumber = params[:any][:anynumber]
+    render :turbo_frame_form, status: :ok, locals: {anynumber: extracted_anynumber, comment: 'turbo_frame_submit ok' }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
 
   get "home/index"
   get "other/index"
+
+  get '/home/turbo_frame_form', as: 'turbo_frame_form'
+  post '/home/turbo_frame_submit', as: 'turbo_frame_submit'
+
   root to: "home#index"
 end
