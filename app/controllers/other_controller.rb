0b1e43707f092c771077edc7e2a46b7f6be6a534
remy727 <remy.wang727@gmail.com>
2022-09-02 12:36:33 -0400

    Feat: add Turbo streams usage

--- app/controllers/other_controller.rb
+++ app/controllers/other_controller.rb
@@ -1,2 +1,7 @@
 class OtherController < ApplicationController
+  def post_something
+    respond_to do |format|
+      format.turbo_stream {  }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   get '/home/turbo_frame_form', as: 'turbo_frame_form'
   post '/home/turbo_frame_submit', as: 'turbo_frame_submit'
 
+  post '/other/post_something', as: 'post_something'
+
   root to: "home#index"
 end
